161a8718296b760342cb248b6f9e25b738918769
SrNacho <fotosydibujos@hotmail.com>
2021-06-02 20:15:43 -0300

    fixed some things

---
+++ excel_rw.py
@@ -0,0 +1,41 @@
+import pytz #libreria para la timezone de todo el mundo
+import pandas as pd #libreria para leer el excel
+from openpyxl import Workbook
+from openpyxl.utils.dataframe import dataframe_to_rows
+from datetime import datetime
+tz_bsas = pytz.timezone('America/Argentina/Buenos_Aires') #timezone de argentina
+
+def start():
+    datetime_bsas = datetime.now(tz_bsas)  # guarda la hora de bsas
+    # formatea la hora y la guarda en la variable hora
+    hora = datetime_bsas.strftime("%d/%m/%Y:%H:%M")
+    #AL FORMATO HAY QUE AÑADIRLE LOS SEGUNDOS ASI NO ESTA TODO EL MINUTO ENTERO ENVIANDO EL MENSAJE HACER ESTO ES LO PROXIMO
+    #POR AHORA ESTA ES LA FUNCIÓN QUE ESTOY EDITANDO, ESTA IGUAL A "start", SOLO POR LO COMENTADO ARRIBA. LO QUE FALTA ES
+    #ACTUALIZAR EL EXCEL UNA VEZ QUE HAYA MANDADO EL MENSAJE. LO ESTOY INTENTANDO EN ./test.py
+    #-----------------FIX-----------------
+    #El error de arriba lo arreglé updateando la fecha del excel que valida la equidad por la nueva fecha, entonces
+    #encuentra la equidad y la cambia.
+    print(hora, "hora actual")
+    xls = pd.read_excel("horarios.xlsx", sheet_name="Hoja 1", )
+    wb = Workbook()
+    ws = wb.active
+    c = -1
+    for col in xls["Dia"]:
+        c = c+1
+        print(col)
+        if str(col) == hora:
+            fecha = xls.at[c, "Dia"]
+            fecha_obj = datetime.strptime(fecha, "%d/%m/%Y:%H:%M")
+            xls.at[c, "Dia"] = datetime.strftime(
+            fecha_obj + pd.Timedelta("2 W"), "%d/%m/%Y:%H:%M")
+            xls = pd.DataFrame(xls)
+            for r in dataframe_to_rows(xls, index=False, header=True):
+                ws.append(r)
+                ws.title = "Hoja 1"
+                wb.save(filename='horarios.xlsx')
+            return (200,str(xls.at[c,"Materia"]),str(xls.at[c,"Hora"])) 
+
+def getDates():
+    xls = pd.read_excel("horarios.xlsx", sheet_name="Hoja 1", )
+    xls = pd.DataFrame(xls)
+    return xls.to_string(index=False)
--- test.py
+++ test.py
@@ -1,19 +1,33 @@
+from numpy import column_stack, load
 import pandas as pd
 from datetime import datetime
 import xlwt
+from openpyxl import load_workbook
+from openpyxl import Workbook
+from openpyxl.utils.dataframe import dataframe_to_rows
 
-xls = pd.read_excel("horarios.xls", sheet_name="Hoja 1")
-for col in xls["Dia"]:
 
-    if str(col) == "06/05/2021:19:30:00":
-        fecha = xls.at[2,"Dia"]
-        fecha_obj = datetime.strptime(fecha,"%d/%m/%Y:%H:%M:%S")
-        xls.at[2,"Dia"] = datetime.strftime(fecha_obj + pd.Timedelta("5 minutes"),"%d/%m/%Y:%H:%M:%S")
-        print(xls.head(3))
-        print("hola")
-xls = pd.DataFrame(xls)
-print(xls)
-xls.to_excel('horarios.xls', sheet_name='Hoja 1', index=False)
+#xls = pd.read_excel("horarios.xls", sheet_name="Hoja 1")
+#xls = load_workbook('./horarios.xlsx')
+#hoja = xls['Hoja 1']
+xls = pd.read_excel("horarios.xlsx", sheet_name="Sheet", )
+df = pd.DataFrame(xls.values)
+wb = Workbook()
+ws = wb.active
+for r in dataframe_to_rows(df, index=False, header=True):
+    ws.append(r)
+ws.title="Hoja 1"
+wb.save(filename = 'horarios.xlsx')
+#for col in xls["Dia"]:
+#     if str(col) == "01/06/2021:01:07":
+#        
+#         fecha = xls.at[2,"Dia"]
+#         fecha_obj = datetime.strptime(fecha,"%d/%m/%Y:%H:%M")
+#         xls.at[2,"Dia"] = datetime.strftime(fecha_obj + pd.Timedelta("5 days"),"%d/%m/%Y:%H:%M:%S")
+#         print("hola")
+# xls = pd.DataFrame(xls)
+# print(xls)
+# xls.to_excel('horarios.xls', sheet_name='Hoja 1', index=False)
 
 #import schedule, time
 #def hola():
